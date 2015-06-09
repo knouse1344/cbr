e191aaff24c06929ab7589364b37a67565f01617
Bryan Knouse <bryan.knouse@gmail.com>
2015-06-09 12:58:39 -0400

    hover states, fonts, investment page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'main/leadership'
   get 'main/programs'
   get 'main/report'
+  get 'main/investment'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
